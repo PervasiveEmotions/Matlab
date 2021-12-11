eaf4de8a3a05652ed794459d6c7e2a1142d5f7aa
imthetuba <90623430+imthetuba@users.noreply.github.com>
2021-12-11 17:33:48 +0100

    Update processFileToMFCCData.m

--- processFileToMFCCData.m
+++ processFileToMFCCData.m
@@ -1,49 +1,97 @@
-function [MFCCDataMap, targetOutputMatrix] = processFileToMFCCData(testDataMap)
+function [MFCCDataMatrix, targetOutputMatrix] = processFileToMFCCData(neutralMatrix, calmMatrix, happyMatrix, sadMatrix, angryMatrix, fearfulMatrix, disgustMatrix, suprisedMatrix)
 
-MFCCDataMap = containers.Map; %create map object
+MFCCDataMatrix = zeros(4046, 1448);
 fs= 48000;
-targetOutputMatrix = zeros(8,1440);
+targetOutputMatrix = zeros(8,1448);
 
 counter = 0;
-for a = 1:8
-    for b = 1:2
-        for c = 1:2
-            for d = 1:2
-                for e = 1:24
-                    counter = counter + 1;
-                    if e < 10 %the last digit(e) has one number, therefore a 0 is added before it
-                        name = "03-01-0"+a+"-0"+b+"-0"+c+"-0"+d+"-0"+e+".wav";%creates a string that corresponds
-                        %to filename
-                        filename = convertStringsToChars(name); %convert string to chars 
-                    
-                    else %last digit(e) has two numbers, no 0 is added before it
-                        name = "03-01-0"+a+"-0"+b+"-0"+c+"-0"+d+"-"+e+".wav";
-                        filename = convertStringsToChars(name);  
-                    end
-                    if not(a == 1 & b== 2)%there are no files with neutral voice and strong intensity, hence this
 
-                        myCell = values(testDataMap,{filename});
-                        rawDataArray = myCell{1,1};
-                        
-                        targetOutputMatrix(a,counter) = 1;
-                        
-                        
-                        coeffs = mfcc(rawDataArray, fs);%FIXME-Window length doesnt work
-                        coeffs = coeffs(:);
-                        MFCCDataMap(filename) = coeffs;
-                        
-                        
-                        
-                    end
-                    
-                end
-                    
-            end
-            
-        end
-        
-    end
-        
+for a = 1:97 % for all of neutral data
+    e = 1;
+    counter = counter + 1;
+    targetOutputMatrix(e,counter) = 1;%set target output in correct row, here row 1, and correct column: the counter
+    
+    y = neutralMatrix(:, a); % extract column at a
+    
+    coeffs = mfcc(y, fs); %do mfcc on the audio vector in the specified file of the matrix
+    coeffs = coeffs(:); %add all coeffs into a single column so it can be put in only one row in the matrix
+    MFCCDataMatrix(:, counter) = coeffs;
 end
-
-MFCCDataMap(:,1) = []; %remove first column which is empty 0s
+for a = 1:193
+    e = 2;
+    counter = counter + 1;
+    targetOutputMatrix(e,counter) = 1;
+    
+    y = calmMatrix(:, a); % extract column at a
+    
+    coeffs = mfcc(y, fs);
+    coeffs = coeffs(:);
+    MFCCDataMatrix(:, counter) = coeffs;
+end
+for a = 1:193
+    e = 3;
+    counter = counter + 1;
+    targetOutputMatrix(e,counter) = 1;
+    
+    y = happyMatrix(:, a); % extract column at a
+    
+    coeffs = mfcc(y, fs);
+    coeffs = coeffs(:);
+    MFCCDataMatrix(:, counter) = coeffs;
+end
+for a = 1:193
+    e = 4;
+    counter = counter + 1;
+    targetOutputMatrix(e,counter) = 1;
+    
+    y = sadMatrix(:, a); % extract column at a
+    
+    coeffs = mfcc(y, fs);
+    coeffs = coeffs(:);
+    MFCCDataMatrix(:, counter) = coeffs;
+end
+for a = 1:193
+    e = 5;
+    counter = counter + 1;
+    targetOutputMatrix(e,counter) = 1;
+    
+    y = angryMatrix(:, a); % extract column at a
+    
+    coeffs = mfcc(y, fs);
+    coeffs = coeffs(:);
+    MFCCDataMatrix(:, counter) = coeffs;
+end
+for a = 1:193
+    e = 6;
+    counter = counter + 1;
+    targetOutputMatrix(e,counter) = 1;
+    
+    y = fearfulMatrix(:, a); % extract column at a
+    
+    coeffs = mfcc(y, fs);
+    coeffs = coeffs(:);
+    MFCCDataMatrix(:, counter) = coeffs;
+end
+for a = 1:193
+    e = 7;
+    counter = counter + 1;
+    targetOutputMatrix(e,counter) = 1;
+    
+    y = disgustMatrix(:, a); % extract column at a
+    
+    coeffs = mfcc(y, fs);
+    coeffs = coeffs(:);
+    MFCCDataMatrix(:, counter) = coeffs;
+    
+end
+for a = 1:193
+    e = 8;
+    counter = counter + 1;
+    targetOutputMatrix(e,counter) = 1;
+    
+    y = suprisedMatrix(:, a); % extract column at a
+    
+    coeffs = mfcc(y, fs);
+    coeffs = coeffs(:);
+    MFCCDataMatrix(:, counter) = coeffs;
+end
