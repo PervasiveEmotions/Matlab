f66c67cb09797edaaf47abcf10009e4173b886b3
imthetuba <90623430+imthetuba@users.noreply.github.com>
2021-12-12 17:16:04 +0100

    Working Version

--- processFileToMFCCData.m
+++ processFileToMFCCData.m
@@ -1,97 +1,80 @@
-function [MFCCDataMatrix, targetOutputMatrix] = processFileToMFCCData(neutralMatrix, calmMatrix, happyMatrix, sadMatrix, angryMatrix, fearfulMatrix, disgustMatrix, suprisedMatrix)
+function [MFCCDataMatrix, targetOutputMatrix] = processFileToMFCCDataSimple(neutralMatrix, happyMatrix, sadMatrix, angryMatrix, fearfulMatrix, disgustMatrix)
 
-MFCCDataMatrix = zeros(4046, 1448);
+MFCCDataMatrix = zeros(4046, 1440);
 fs= 48000;
-targetOutputMatrix = zeros(8,1448);
+targetOutputMatrix = zeros(6,1440);
 
 counter = 0;
 
-for a = 1:97 % for all of neutral data
+for a = 1:96
     e = 1;
     counter = counter + 1;
-    targetOutputMatrix(e,counter) = 1;%set target output in correct row, here row 1, and correct column: the counter
+    targetOutputMatrix(e,counter) = 1;
     
     y = neutralMatrix(:, a); % extract column at a
     
-    coeffs = mfcc(y, fs); %do mfcc on the audio vector in the specified file of the matrix
-    coeffs = coeffs(:); %add all coeffs into a single column so it can be put in only one row in the matrix
+    coeffs = mfcc(y, fs);  %calcualte mfcc
+    coeffs = coeffs';      %transpose the matrix
+    coeffs = reshape(coeffs,[],1);  %reshape it to have a matrix of the size: input_features (6972) x 1. i.e. all values in one column
     MFCCDataMatrix(:, counter) = coeffs;
 end
-for a = 1:193
+for a = 1:192
     e = 2;
     counter = counter + 1;
     targetOutputMatrix(e,counter) = 1;
     
-    y = calmMatrix(:, a); % extract column at a
+    y = disgustMatrix(:, a); % extract column at a
     
-    coeffs = mfcc(y, fs);
-    coeffs = coeffs(:);
+    coeffs = mfcc(y, fs);  %calcualte mfcc
+    coeffs = coeffs';      %transpose the matrix
+    coeffs = reshape(coeffs,[],1);  %reshape it to have a matrix of the size: input_features (6972) x 1. i.e. all values in one column
     MFCCDataMatrix(:, counter) = coeffs;
 end
-for a = 1:193
+for a = 1:192
     e = 3;
     counter = counter + 1;
     targetOutputMatrix(e,counter) = 1;
     
     y = happyMatrix(:, a); % extract column at a
     
-    coeffs = mfcc(y, fs);
-    coeffs = coeffs(:);
+    coeffs = mfcc(y, fs);  %calcualte mfcc
+    coeffs = coeffs';      %transpose the matrix
+    coeffs = reshape(coeffs,[],1);  %reshape it to have a matrix of the size: input_features (6972) x 1. i.e. all values in one column
     MFCCDataMatrix(:, counter) = coeffs;
 end
-for a = 1:193
+for a = 1:192
     e = 4;
     counter = counter + 1;
     targetOutputMatrix(e,counter) = 1;
     
     y = sadMatrix(:, a); % extract column at a
     
-    coeffs = mfcc(y, fs);
-    coeffs = coeffs(:);
+    coeffs = mfcc(y, fs);  %calcualte mfcc
+    coeffs = coeffs';      %transpose the matrix
+    coeffs = reshape(coeffs,[],1);  %reshape it to have a matrix of the size: input_features (6972) x 1. i.e. all values in one column
     MFCCDataMatrix(:, counter) = coeffs;
 end
-for a = 1:193
+for a = 1:192
     e = 5;
     counter = counter + 1;
     targetOutputMatrix(e,counter) = 1;
     
     y = angryMatrix(:, a); % extract column at a
     
-    coeffs = mfcc(y, fs);
-    coeffs = coeffs(:);
+    coeffs = mfcc(y, fs);  %calcualte mfcc
+    coeffs = coeffs';      %transpose the matrix
+    coeffs = reshape(coeffs,[],1);  %reshape it to have a matrix of the size: input_features (6972) x 1. i.e. all values in one column
     MFCCDataMatrix(:, counter) = coeffs;
 end
-for a = 1:193
+for a = 1:192
     e = 6;
     counter = counter + 1;
     targetOutputMatrix(e,counter) = 1;
     
     y = fearfulMatrix(:, a); % extract column at a
     
-    coeffs = mfcc(y, fs);
-    coeffs = coeffs(:);
-    MFCCDataMatrix(:, counter) = coeffs;
-end
-for a = 1:193
-    e = 7;
-    counter = counter + 1;
-    targetOutputMatrix(e,counter) = 1;
-    
-    y = disgustMatrix(:, a); % extract column at a
-    
-    coeffs = mfcc(y, fs);
-    coeffs = coeffs(:);
-    MFCCDataMatrix(:, counter) = coeffs;
-    
-end
-for a = 1:193
-    e = 8;
-    counter = counter + 1;
-    targetOutputMatrix(e,counter) = 1;
-    
-    y = suprisedMatrix(:, a); % extract column at a
-    
-    coeffs = mfcc(y, fs);
-    coeffs = coeffs(:);
+    coeffs = mfcc(y, fs);  %calcualte mfcc
+    coeffs = coeffs';      %transpose the matrix
+    coeffs = reshape(coeffs,[],1);  %reshape it to have a matrix of the size: input_features (6972) x 1. i.e. all values in one column
     MFCCDataMatrix(:, counter) = coeffs;
 end
